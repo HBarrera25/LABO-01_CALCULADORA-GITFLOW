21230a327bafeb62d81f1885c4b481b268686b4b
HBarrera25 <00226325@uca.edu.sv>
2026-01-09 17:53:39 -0600

    add: Menu Interactivo Calculadora

--- calculadora.cpp
+++ calculadora.cpp
@@ -7,3 +7,22 @@ float resta(float a, float b);
 float multiplicacion(float a, float b);
 float division(float a, float b);
 
+int main(){
+    int opcion;
+    float num1, num2;
+
+    do{
+        cout << "===========BIENVENIDO A LA CALCULADORA DE OPERACIONES=======" << endl << endl;
+        cout << "Seleccione la operacion que desea realizar: " << endl;
+        cout << "1. Suma" << endl;
+        cout << "2. Resta" << endl;
+        cout << "3. Multiplicacion" << endl;
+        cout << "4. Division" << endl;
+        cout << "0. Salir" << endl;
+        cin >> opcion;
+
+      
+    }
+    
+}
+
