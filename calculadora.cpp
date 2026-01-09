678d2fa12fce86ebcb833ec7ab29e2d9574f1ba0
HBarrera25 <00226325@uca.edu.sv>
2026-01-09 17:22:44 -0600

    add: Declaracion prototipo funciones

---
+++ calculadora.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+float suma(float a, float b);
+float resta(float a, float b);
+float multiplicacion(float a, float b);
+float division(float a, float b);
+
